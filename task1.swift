a6c22ce48a71715861c962f8ef3e86a0f399d307
Tomasz <piotrowskitomasz94@gmail.com>
2020-01-05 12:56:48 +0100

    added task1 file

---
+++ task1.swift
@@ -0,0 +1,56 @@
+import Foundation
+import Glibc
+
+class CheckListElement: CustomStringConvertible {
+
+    var textField: String = "";
+    var isChecked: Bool = false;
+
+    init(textField: String, isChecked: Bool) {
+        self.textField = textField
+        self.isChecked = isChecked
+    }
+
+    func description(textField: String, isChecked: Bool) {
+
+        if isChecked == false {
+            print(textField + " -> " + "Do Wykonania")
+        }
+        else {
+            print(textField + " -> " + "Gotowe")
+        }
+    }
+
+    func descriptionWithoutParameters() {
+
+        if isChecked == false {
+            print(textField + " -> " + "Do Wykonania")
+        }
+        else {
+            print(textField + "->" + "Gotowe")
+        }
+    }
+
+    var description: String {
+      
+        var description = ""
+        description += "\(self.textField)"
+        description += " -> "
+        if isChecked == false {
+            description += "Do Wykonania"
+
+        }
+        else {
+            description += "Gotowe"
+        }
+        return description
+    }
+    
+}
+
+let object = CheckListElement(textField: "Poniedziałek Zrobić Pranie", isChecked: false)
+object.descriptionWithoutParameters()
+object.description(textField: "Wtorek Zrobić Pranie", isChecked: false)
+
+let objectWithProtocol = CheckListElement(textField: "Środa Zrobić Pranie", isChecked: false)
+print(objectWithProtocol)
