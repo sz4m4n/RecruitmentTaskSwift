366622646ce3fa59364060ada75cb6ba9769b9c8
Tomasz <piotrowskitomasz94@gmail.com>
2020-01-05 13:25:07 +0100

    added task2 file

---
+++ task2.swift
@@ -0,0 +1,68 @@
+import Foundation
+import Glibc
+
+class CheckListElement: CustomStringConvertible {
+
+    var textField: String = "";
+    var dayOfTheWeek: String = "";
+    var isChecked: Bool = true;
+
+    init(dayOfTheWeek: String, textField: String, isChecked: Bool) {
+        self.dayOfTheWeek = dayOfTheWeek
+        self.textField = textField
+        self.isChecked = isChecked
+    }
+
+    func description(dayOfTheWeek: String, textField: String) {
+    
+        let result = dayOfTheWeek.range( of: "[a-zA-Z]", options: .regularExpression) != nil
+        
+        if result == true {
+            if isChecked == false {
+                print(dayOfTheWeek + " " + textField + " -> " + "Do Wykonania")
+            } else {
+                print(dayOfTheWeek + " " + textField + " -> " + "Gotowe")
+            }
+        } else {
+            print("Incorrect name of the day")
+        }
+    }
+
+    func descriptionWithoutParameters() {
+
+        let result = dayOfTheWeek.range( of: "[a-zA-Z]", options: .regularExpression) != nil
+        
+        if result == true {
+            if isChecked == false {
+                print(dayOfTheWeek + " " + textField + " -> " + "Do Wykonania")
+            } else {
+                print(dayOfTheWeek + " " + textField + " -> " + "Gotowe")
+            }
+        } else {
+            print("Incorrect name of the day")
+        }
+    }
+
+    var description: String {
+
+        var description = ""
+        description += "\(self.dayOfTheWeek) "
+        description += "\(self.textField)"
+        description += " -> "
+        
+        if isChecked == false {
+            description += "Do Wykonania"
+        } else {
+            description += "Gotowe"
+        }
+        return description
+    }
+
+}
+
+let object = CheckListElement(dayOfTheWeek: "Sobota", textField: "Wyjść na spacer", isChecked: true)
+object.descriptionWithoutParameters()
+object.description(dayOfTheWeek: "Sobota", textField: "Zrobić zakupy")
+
+let objectWithProtocol = CheckListElement(dayOfTheWeek: "Niedziela", textField: "Wrócić do domu", isChecked: true)
+print(objectWithProtocol)
